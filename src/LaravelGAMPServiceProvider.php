00df9139bc14f97112671d02b7b724f24703b0d4
Irfaq Syed <irazasyed@users.noreply.github.com>
2020-04-30 15:12:50 +0530

    Bind gamp instead of singleton.

--- src/LaravelGAMPServiceProvider.php
+++ src/LaravelGAMPServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Irazasyed\LaravelGAMP;
 
-use Illuminate\Contracts\Container\Container as Application;
 use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Lumen\Application as LumenApplication;
@@ -11,33 +10,16 @@ use TheIconic\Tracking\GoogleAnalytics\Analytics;
 class LaravelGAMPServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
-     * Bootstrap the application events.
+     * Boot the service provider.
      */
     public function boot()
     {
-        $this->setupConfig($this->app);
-    }
+        $source = __DIR__ . '/config/gamp.php';
 
-    /**
-     * Setup the config.
-     *
-     * @param \Illuminate\Contracts\Container\Container $app
-     */
-    protected function setupConfig(Application $app)
-    {
-        $source = __DIR__.'/config/gamp.php';
-
-        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('gamp.php')]);
-        } elseif ($app instanceof LumenApplication) {
-            $app->configure('gamp');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('gamp');
         }
 
         $this->mergeConfigFrom($source, 'gamp');
@@ -48,17 +30,7 @@ class LaravelGAMPServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerAnalytics($this->app);
-    }
-
-    /**
-     * Initialize Analytics Library with Default Config.
-     *
-     * @param \Illuminate\Contracts\Container\Container $app
-     */
-    protected function registerAnalytics(Application $app)
-    {
-        $app->singleton('gamp', function ($app) {
+        $this->app->bind(Analytics::class, function ($app) {
             $config = $app['config'];
 
             $analytics = new Analytics($config->get('gamp.is_ssl', false), $config->get('gamp.is_disabled', false));
@@ -77,7 +49,7 @@ class LaravelGAMPServiceProvider extends ServiceProvider
             return $analytics;
         });
 
-        $app->alias('gamp', Analytics::class);
+        $this->app->alias(Analytics::class, 'gamp');
     }
 
     /**
@@ -87,6 +59,6 @@ class LaravelGAMPServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['gamp', Analytics::class];
+        return [Analytics::class, 'gamp'];
     }
 }
